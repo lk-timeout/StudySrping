5c651bba57cd7b7526e0c533f7f8d8d778f8371b
timeout <lk.timeout@gmail.com>
2021-08-25 22:39:50 +0800

    增加hello程序

---
+++ hello-spring/src/test/java/lk/spring/hello/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.spring.hello.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
